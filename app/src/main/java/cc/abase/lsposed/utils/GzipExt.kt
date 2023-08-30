47fbc16104436982ff7b926ac1e61b8c91ef7bcf
XXX <XXX@qq.com>
2023-08-30 19:16:28 +0700

    GZIP加解密修复

--- app/src/main/java/cc/abase/lsposed/utils/GzipExt.kt
+++ app/src/main/java/cc/abase/lsposed/utils/GzipExt.kt
@@ -1,15 +1,14 @@
 package cc.abase.lsposed.utils
 
+import android.util.Base64
 import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 
-fun String.gzip(): ByteArray {
-  return ByteArrayOutputStream().apply {
-    GZIPOutputStream(this).bufferedWriter().use { it.write(this@gzip) }
-  }.toByteArray()
+fun String.gzip(): String {
+  return Base64.encodeToString(ByteArrayOutputStream().also { bos -> GZIPOutputStream(bos).use { it.write(this.toByteArray()) } }.toByteArray(), Base64.NO_WRAP)
 }
 
-fun ByteArray.ungzip(): String {//TODO XX
-  return GZIPInputStream(this.inputStream()).bufferedReader().use { it.readText() }
+fun String.ungzip(): String {
+  return String(GZIPInputStream(Base64.decode(this, Base64.NO_WRAP).inputStream()).use { it.readBytes() }, Charsets.UTF_8)
 }
